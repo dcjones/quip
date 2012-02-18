0c41a211b1c22195e133301df6e729c663852bdf
Daniel Jones <dcjones@cs.washington.edu>
2012-02-18 01:20:43 -0800

    Including cumulative deltas in the quality encoder.

--- src/qualenc.c
+++ src/qualenc.c
@@ -12,31 +12,34 @@
 static const char   qual_last  = 40;
 static const size_t qual_size  = 41;
 static const size_t pos_bins   = 8;
-static const size_t mu_bins    = 16;
+static const size_t mu_bins    = 12;
+static const size_t q2_bins    = 12;
+static const size_t delta_bins = 8;
+static const int    delta_max  = 41;
 
 
 struct qualenc_t_
 {
     ac_t* ac;
     cond_dist41_t cs;
-    dist41_t ms;
+    dist16_t ms;
 };
 
 
-/* index into the 'cs' conditional distribution */
-#define cs_index(n, i, mu, q2, q1) \
+#define cs_index(n, i, delta, mu, q2, q1) \
     (q1 + qual_size * (\
-            q2 + qual_size * (\
-                mu + mu_bins * (\
-                    (i * pos_bins) / n))))
+            (qual_size - q2 >= q2_bins ? q2_bins - 1 : (qual_size - q2)) + q2_bins * (\
+                (delta * delta_bins / delta_max) + delta_bins * (\
+                    (i * pos_bins) / n + pos_bins * (\
+                        mu)))))
+
 
 
 static void qualenc_init(qualenc_t* E, bool decode)
 {
-    cond_dist41_init(&E->cs, pos_bins * mu_bins * qual_size * qual_size, decode);
+    cond_dist41_init(&E->cs, delta_bins * pos_bins * mu_bins * q2_bins * qual_size, decode);
     cond_dist41_set_update_rate(&E->cs, 4);
-
-    dist41_init(&E->ms, decode);
+    dist16_init(&E->ms, decode);
 }
 
 
@@ -65,7 +68,7 @@ qualenc_t* qualenc_alloc_decoder(quip_reader_t reader, void* reader_data)
 void qualenc_free(qualenc_t* E)
 {
     cond_dist41_free(&E->cs);
-    dist41_free(&E->ms);
+    dist16_free(&E->ms);
     ac_free(E->ac);
     free(E);
 }
@@ -76,6 +79,10 @@ static inline char charmax2(char a, char b)
     return a > b ? a : b;
 }
 
+static inline int intmin2(int a, int b)
+{
+    return a < b ? a : b;
+}
 
 
 void qualenc_encode(qualenc_t* E, const seq_t* x)
@@ -83,6 +90,7 @@ void qualenc_encode(qualenc_t* E, const seq_t* x)
     unsigned char q;   /* quality score */
     unsigned char q1;  /* preceding quality score */
     unsigned char q2;  /* maximum of three quality scores preceding q1 */
+    int delta = 0; 
 
     char* qs = x->qual.s;
     size_t n = x->qual.n;
@@ -93,8 +101,8 @@ void qualenc_encode(qualenc_t* E, const seq_t* x)
     for (i = 0; i < n; ++i) mu += qs[i];
     mu /= n;
 
-    mu = (mu * mu_bins) / (qual_size + 1);
-    dist41_encode(E->ac, &E->ms, mu);
+    mu = (mu * mu_bins) / qual_size;
+    dist16_encode(E->ac, &E->ms, mu);
 
 
     /* I am using the previous 4 positions to predict the next quality score.
@@ -104,17 +112,28 @@ void qualenc_encode(qualenc_t* E, const seq_t* x)
 
     /* case: i = 0 */
     if (n >= 1) {
-        cond_dist41_encode(E->ac, &E->cs, cs_index(n, 0, mu, 0, 0), qs[0]);
+        cond_dist41_encode(E->ac, &E->cs,
+                cs_index(n, 0, delta, mu, 0, 0), qs[0]);
     }
 
     /* case: i = 1 */
     if (n >= 2) {
-        cond_dist41_encode(E->ac, &E->cs, cs_index(n, 1, mu, 0, qs[0]), qs[1]);
+        cond_dist41_encode(E->ac, &E->cs,
+                cs_index(n, 1, delta, mu, 0, qs[0]), qs[1]);
+
+        if (qs[0] > qs[1]) {
+            delta = intmin2(delta_max - 1, delta + qs[0] - qs[1]);
+        }
     }
 
     /* case: i = 2 */
     if (n >= 3) {
-        cond_dist41_encode(E->ac, &E->cs, cs_index(n, 2, mu, qs[0], qs[1]), qs[2]);
+        cond_dist41_encode(E->ac, &E->cs,
+                cs_index(n, 2, delta, mu, qs[0], qs[1]), qs[2]);
+
+        if (qs[1] > qs[2]) {
+            delta = intmin2(delta_max - 1, delta + qs[1] - qs[2]);
+        }
     }
 
     /* case: i >= 4 */
@@ -123,7 +142,12 @@ void qualenc_encode(qualenc_t* E, const seq_t* x)
         q1 = qs[i - 1];
         q2 = charmax2(qs[i - 2], qs[i - 3]);
 
-        cond_dist41_encode(E->ac, &E->cs, cs_index(n, i, mu, q2, q1), q);
+        cond_dist41_encode(E->ac, &E->cs,
+                cs_index(n, i, delta, mu, q2, q1), q);
+
+        if (qs[i - 1] > qs[i]) {
+            delta = intmin2(delta_max - 1, delta + qs[i - 1] - qs[i]);
+        }
     }
 }
 
@@ -143,8 +167,9 @@ void qualenc_decode(qualenc_t* E, seq_t* seq, size_t n)
 
     unsigned char q1;  /* preceding quality score */
     unsigned char q2;  /* maximum of three quality scores preceding q1 */
+    int delta = 0; 
 
-    uint32_t mu = dist41_decode(E->ac, &E->ms);
+    uint32_t mu = dist16_decode(E->ac, &E->ms);
 
     size_t i;
 
@@ -152,21 +177,29 @@ void qualenc_decode(qualenc_t* E, seq_t* seq, size_t n)
     if (n >= 1) {
         qs[qual->n++] =
             cond_dist41_decode(E->ac, &E->cs,
-                    cs_index(n, 0, mu, 0, 0));
+                    cs_index(n, 0, delta, mu, 0, 0));
     }
 
     /* case: i = 1 */
     if (n >= 2) {
         qs[qual->n++] =
             cond_dist41_decode(E->ac, &E->cs,
-                    cs_index(n, 0, mu, 0, qs[0]));
+                    cs_index(n, 0, delta, mu, 0, qs[0]));
+
+        if (qs[0] > qs[1]) {
+            delta = intmin2(delta_max - 1, delta + qs[0] - qs[1]);
+        }
     }
 
     /* case: i = 2 */
     if (n >= 3) {
         qs[qual->n++] =
             cond_dist41_decode(E->ac, &E->cs,
-                    cs_index(n, 0, mu, qs[0], qs[1]));
+                    cs_index(n, 0, delta, mu, qs[0], qs[1]));
+
+        if (qs[1] > qs[2]) {
+            delta = intmin2(delta_max - 1, delta + qs[1] - qs[2]);
+        }
     }
 
 
@@ -178,7 +211,13 @@ void qualenc_decode(qualenc_t* E, seq_t* seq, size_t n)
                 qual->s[qual->n - 3]);
 
         qs[qual->n++] =
-            cond_dist41_decode(E->ac, &E->cs, cs_index(n, i, mu, q2, q1));
+            cond_dist41_decode(E->ac, &E->cs,
+                    cs_index(n, i, delta, mu, q2, q1));
+
+        if (qs[i - 1] > qs[i]) {
+            delta = intmin2(delta_max - 1, delta + qs[i - 1] - qs[i]);
+        }
+
         ++i;
     }
 
