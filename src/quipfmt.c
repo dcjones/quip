dcjones/quip

commit b8b5528d4c1818479b2fd22547d56fef8e2ff5c8

Do not use assembly-based compression by default. Enable with the -a switch.

src/quipfmt.c
@@ -298,8 +298,8 @@ quip_quip_out_t* quip_quip_out_open(
 {
     quip_quip_out_t* C = malloc_or_die(sizeof(quip_quip_out_t));
 
-    bool quick = (opts & QUIP_OPT_QUIP_ASSEMBLY_FREE) != 0;
-    bool ref_based = ref != NULL;
+    bool assembly_based = (opts & QUIP_OPT_QUIP_ASSEMBLY) != 0;
+    bool ref_based      = ref != NULL;
     C->writer = writer;
     C->writer_data = writer_data;
     C->ref = ref;
@@ -340,7 +340,7 @@ quip_quip_out_t* quip_quip_out_open(
     C->idenc     = idenc_alloc_encoder(writer, (void*) writer_data);
     C->auxenc    = samoptenc_alloc_encoder(writer, (void*) writer_data);
     C->qualenc   = qualenc_alloc_encoder(writer, (void*) writer_data);
-    C->assembler = assembler_alloc(writer, (void*) writer_data, quick, ref);
+    C->assembler = assembler_alloc(writer, (void*) writer_data, assembly_based, ref);
 
     /* write header */
     C->writer(C->writer_data, quip_header_magic, 6);
@@ -348,8 +348,8 @@ quip_quip_out_t* quip_quip_out_open(
 
     /* header flags */
     uint8_t header_flags = 0;
-    if (ref_based) header_flags |= QUIP_FLAG_REFERENCE;
-    if (!quick)    header_flags |= QUIP_FLAG_ASSEMBLED;
+    if (ref_based)      header_flags |= QUIP_FLAG_REFERENCE;
+    if (assembly_based) header_flags |= QUIP_FLAG_ASSEMBLED;
     C->writer(C->writer_data, &header_flags, 1);
 
     /* write reference hash */
@@ -934,8 +934,8 @@ quip_quip_in_t* quip_quip_in_open(
 
     check_header_version(header_version);
 
-    bool quick     = (header_flags & QUIP_FLAG_ASSEMBLED) == 0;
-    bool ref_based = (header_flags & QUIP_FLAG_REFERENCE) != 0;
+    bool assembly_based = (header_flags & QUIP_FLAG_ASSEMBLED) == 0;
+    bool ref_based      = (header_flags & QUIP_FLAG_REFERENCE) != 0;
 
     if (ref_based) {
         if (ref == NULL) {
@@ -959,7 +959,7 @@ quip_quip_in_t* quip_quip_in_open(
 
     D->idenc   = idenc_alloc_decoder(id_buf_reader, (void*) D);
     D->auxenc  = samoptenc_alloc_decoder(aux_buf_reader, (void*) D);
-    D->disassembler = disassembler_alloc(seq_buf_reader, (void*) D, quick, ref);
+    D->disassembler = disassembler_alloc(seq_buf_reader, (void*) D, assembly_based, ref);
     D->qualenc = qualenc_alloc_decoder(qual_buf_reader, (void*) D);
 
     return D;

src/assembler.c
@@ -43,8 +43,8 @@ static const size_t assembly_n = 2500000;
 
 struct assembler_t_
 {
-    /* don't actually assemble anything */
-    bool quick;
+    /* actually assemble something */
+    bool assemble;
 
     /* function to write compressed output */
     quip_writer_t writer;
@@ -89,13 +89,13 @@ static void build_kmer_hash(assembler_t* A);
 assembler_t* assembler_alloc(
         quip_writer_t   writer,
         void*           writer_data,
-        bool            quick,
+        bool            assemble,
         const seqmap_t* ref)
 {
     assembler_t* A = malloc_or_die(sizeof(assembler_t));
     memset(A, 0, sizeof(assembler_t));
 
-    A->quick = quick;
+    A->assemble = assemble;
 
     A->writer = writer;
     A->writer_data = writer_data;
@@ -103,7 +103,7 @@ assembler_t* assembler_alloc(
 
     /* If we are not assembling, we do not need any of the data structure
      * initialized below. */
-    if (!quick) {
+    if (assemble) {
         A->seeds = malloc_or_die(seeds_n * sizeof(twobit_t*));
         memset(A->seeds, 0, seeds_n * sizeof(twobit_t*));
         A->seeds_len = 0;
@@ -445,7 +445,7 @@ void assembler_add_seq(assembler_t* A, const short_read_t* seq)
         return;
     }
 
-    if (A->quick) {
+    if (!A->assemble) {
         seqenc_encode_char_seq(A->seqenc, seq->seq.s, seq->seq.n);
     }
     else if (A->assembly_pending_n > 0) {
@@ -496,7 +496,7 @@ size_t assembler_finish(assembler_t* A)
     /* This leads to slightly better compression, but slows things
        down a lot on large assemblies. */
 /*
-    if (!A->quick && A->assembly_pending_n == 0) {
+    if (A->assemble && A->assembly_pending_n == 0) {
         seqenc_get_supercontig_consensus(A->seqenc, A->supercontig);
         index_contigs(A);
     }
@@ -523,12 +523,10 @@ void assembler_flush(assembler_t* A)
 }
 
 
-/* TODO: restructure disassembler */
-
 struct disassembler_t_
 {
-    /* don't actually assemble anything */
-    bool quick;
+    /* actually assemble something */
+    bool assemble;
 
     /* function to read compressed input */
     quip_reader_t reader;
@@ -565,7 +563,7 @@ struct disassembler_t_
 disassembler_t* disassembler_alloc(
     quip_reader_t reader,
     void* reader_data,
-    bool quick,
+    bool assemble,
     const seqmap_t* ref)
 {
     disassembler_t* D = malloc_or_die(sizeof(disassembler_t));
@@ -575,10 +573,10 @@ disassembler_t* disassembler_alloc(
     D->reader = reader;
     D->reader_data = reader_data;
     D->ref = ref;
-    D->quick = quick;
+    D->assemble = assemble;
     D->initial_state = true;
 
-    if (!quick) {
+    if (assemble) {
         D->seeds = malloc_or_die(seeds_n * sizeof(twobit_t*));
         memset(D->seeds, 0, seeds_n * sizeof(twobit_t*));
         D->seeds_len = 0;

src/assembler.h
@@ -27,7 +27,7 @@ typedef struct assembler_t_ assembler_t;
 assembler_t* assembler_alloc(
         quip_writer_t   writer,
         void*           writer_data,
-        bool            quick,
+        bool            assemble,
         const seqmap_t* ref);
 
 void assembler_free(assembler_t*);
@@ -45,7 +45,7 @@ typedef struct disassembler_t_ disassembler_t;
 disassembler_t* disassembler_alloc(
     quip_reader_t reader,
     void* reader_data,
-    bool quick,
+    bool  assemble,
     const seqmap_t* ref);
 
 void disassembler_free(disassembler_t*);

src/quip.c
@@ -30,9 +30,9 @@
 #define O_BINARY 0
 #endif 
 
-static bool force_flag  = false;
-static bool quick_flag  = false;
-static bool stdout_flag = false;
+static bool force_flag    = false;
+static bool assembly_flag = false;
+static bool stdout_flag   = false;
 
 static enum {
     QUIP_CMD_CONVERT,
@@ -312,7 +312,7 @@ static int quip_cmd_convert(char** fns, size_t fn_count)
         in  = quip_in_open(block_reader,  (void*) stdin,  in_fmt, 0, ref);
         quip_get_aux(in, &aux);
 
-        if (out_fmt == QUIP_FMT_QUIP && quick_flag) opts = QUIP_OPT_QUIP_ASSEMBLY_FREE;
+        if (out_fmt == QUIP_FMT_QUIP && assembly_flag) opts = QUIP_OPT_QUIP_ASSEMBLY;
         else opts = 0;
 
         out = quip_out_open(block_writer, (void*) stdout, out_fmt, opts, &aux, ref);
@@ -411,7 +411,7 @@ static int quip_cmd_convert(char** fns, size_t fn_count)
                 free(out_fn);
             }
 
-            if (out_fmt == QUIP_FMT_QUIP && quick_flag) opts = QUIP_OPT_QUIP_ASSEMBLY_FREE;
+            if (out_fmt == QUIP_FMT_QUIP && assembly_flag) opts = QUIP_OPT_QUIP_ASSEMBLY;
             else opts = 0;
 
             out = quip_out_open(block_writer, (void*) fout, out_fmt, opts, &aux, ref);
@@ -556,9 +556,9 @@ int main(int argc, char* argv[])
         {"output",     required_argument, NULL, 'o'},
         {"to",         required_argument, NULL, 'o'},
         {"reference",  required_argument, NULL, 'r'},
+        {"assembly",   no_argument, NULL, 'a'},
         {"list",       no_argument, NULL, 'l'},
         {"test",       no_argument, NULL, 't'},
-        {"quick",      no_argument, NULL, 'q'},
         {"stdout",     no_argument, NULL, 'c'},
         {"decompress", no_argument, NULL, 'd'},
         {"uncompress", no_argument, NULL, 'd'},
@@ -589,7 +589,7 @@ int main(int argc, char* argv[])
     }
     
     while (1) {
-        opt = getopt_long(argc, argv, "i:o:r:ltqcdfvhV", long_options, &opt_idx);
+        opt = getopt_long(argc, argv, "i:o:r:ltacdfvhV", long_options, &opt_idx);
 
         if (opt == -1) break;
 
@@ -617,8 +617,8 @@ int main(int argc, char* argv[])
                 out_fmt = QUIP_FMT_NULL;
                 break;
 
-            case 'q':
-                quick_flag = true;
+            case 'a':
+                assembly_flag = true;
                 break;
 
             case 'c':

src/quip.h
@@ -193,9 +193,9 @@ typedef enum quip_fmt_t_
  * combinations of these flags.
  */
 
-/* Compress quip files without the de novo assembly step, which
- * is somewhat faster. */
-#define QUIP_OPT_QUIP_ASSEMBLY_FREE 1
+/* Compress quip files with a de novo assembly step: better compression
+ * at the cost of compression and decompression speed. */
+#define QUIP_OPT_QUIP_ASSEMBLY 1
 
 /* Output SAM files in BAM (compressed SAM) format. */
 #define QUIP_OPT_SAM_BAM 1

commit c40a6e66355a7628d90bf7e7d9040adeed26670e

Fix a decomp. bug introduced in b8b5528d4c1818479b2fd22547d56fef8e2ff5c8.

src/quipfmt.c
@@ -937,7 +937,7 @@ quip_quip_in_t* quip_quip_in_open(
 
     check_header_version(header_version);
 
-    bool assembly_based = (header_flags & QUIP_FLAG_ASSEMBLED) == 0;
+    bool assembly_based = (header_flags & QUIP_FLAG_ASSEMBLED) != 0;
     bool ref_based      = (header_flags & QUIP_FLAG_REFERENCE) != 0;
 
     if (ref_based) {